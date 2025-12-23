6863e6d4c2e18bebdbe14eb90f3f9eca1d92fce3
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2025-12-23 16:21:19 -0300

    feat: adicionar a classe `VideoTrack`

--- src/subedit/info.py
+++ src/subedit/info.py
@@ -21,6 +21,54 @@ class Attachment(IAttachment):
         return self.__file_name
 
 
+class AudioTrack(ITrack):
+    def __init__(self, data: ITrackAdapter):
+        self.__id = data.id()
+        self.__uid = data.uid()
+        self.__codec = data.codec()
+        self.__codec_id = data.codec_id()
+        self.__default_track = data.default_track()
+        self.__enabled_track = data.enabled_track()
+        self.__forced_track = data.forced_track()
+        self.__language = data.language()
+        self.__language_ietf = data.language_ietf()
+        self.__number = data.number()
+        self.__track_name = data.track_name()
+
+    def id(self) -> int:
+        return self.__id
+
+    def uid(self) -> int:
+        return self.__uid
+
+    def codec(self) -> str:
+        return self.__codec
+
+    def codec_id(self) -> str:
+        return self.__codec_id
+
+    def default_track(self) -> bool:
+        return self.__default_track
+
+    def enabled_track(self) -> bool:
+        return self.__enabled_track
+
+    def forced_track(self) -> bool:
+        return self.__forced_track
+
+    def language(self) -> str:
+        return self.__language
+
+    def language_ietf(self) -> str:
+        return self.__language_ietf
+
+    def number(self) -> int:
+        return self.__number
+
+    def track_name(self) -> str:
+        return self.__track_name
+
+
 class SubtitleTrack(ITrack):
     def __init__(self, data: ITrackAdapter):
         self.__id = data.id()
