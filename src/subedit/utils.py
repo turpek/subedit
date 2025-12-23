d1b4d47e7ce2604beecf4a3d27fd5cca64a2b838
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2025-12-23 20:20:30 -0300

    style: adicionar 2 linhas antes da classe

--- src/subedit/utils.py
+++ src/subedit/utils.py
@@ -7,28 +7,36 @@ SUFFIX = ['.mkv']
 
 logger.add(f"{PATH_LOG}/subedit.log", rotation="10 MB")
 
-def check_output(path: str|Path):
+
+def check_output(path: str | Path):
     output_dir = Path(path) / 'output'
     if not output_dir.exists():
         logger.info("creating the output directory")
         output_dir.mkdir()
     return output_dir
 
+
 class PrettyMsg:
     def __init__(self, index, size, digits):
         self.__index = index
         self.__size = size
         self.__digits = digits
+
     def __repr__(self):
         return f'PrettyMsg(index="{self.__index}", size="{self.__size}", digits="{self.__digits}")'
+
     def __str__(self):
         return f'[{self.__index:>{self.__digits}}/{self.__size}]'
+
     def __len__(self):
         return self.__size
+
     def index(self):
         return self.__index
+
     def digits(self):
         return self.__digits
+
 
 class PrettyCount:
     def __init__(self, list_: list, start=1):
@@ -40,4 +48,3 @@ class PrettyCount:
     def __iter__(self):
         for i, el in enumerate(self.__list, start=self.__start):
             yield PrettyMsg(i, self.__size, self.__digits), el
-
