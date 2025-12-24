cdf8d4295beb99014a563c832625947b7b4ce31f
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2025-12-24 04:54:27 -0300

    feat: criar a classe estática `PathBuilder`
    `PathBuilder` é um construtor de `path` para os assets.

---
+++ src/subedit/path_builder.py
@@ -0,0 +1,23 @@
+from pathlib3x import Path
+from subedit.custom_exceptions import UnsupportedAssetError
+from subedit.interface import Asset
+from subedit.info import Attachment, Track
+
+
+class PathBuilder:
+
+    @staticmethod
+    def builder(asset: Asset) -> Path:
+
+        if isinstance(asset, Track):
+            path = Path('tracks')
+        elif isinstance(asset, Attachment):
+            path = Path('fonts')
+        else:
+            asset_type = type(asset).__name__
+            raise UnsupportedAssetError(
+                f"Cannot build path for asset of type '{asset_type}'. "
+                "Supported assets: Track, Attachment."
+            )
+
+        return path / asset.file_name()
