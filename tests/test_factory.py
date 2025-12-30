4fd2ccae39875b85f7fad653f3922b9ce41b90b6
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2025-12-30 03:53:46 -0300

    test: criar os testes para a `MKVMergeFactory`

---
+++ tests/test_factory.py
@@ -0,0 +1,74 @@
+from pathlib3x import Path
+from pytest import fixture
+from subedit.factory import MKVMergeFactory
+from subedit.utils import MediaType
+from subprocess import PIPE
+
+
+@fixture
+def mkvmerge_deps(mocker):
+    return {
+        'AttachmentAdapter': mocker.patch('subedit.factory.MKVMergeAttachmentAdapter'),
+        'TrackAdapter': mocker.patch('subedit.factory.MKVMergeTrackAdapter'),
+        'Attachment': mocker.patch('subedit.factory.Attachment'),
+        'Audio': mocker.patch('subedit.factory.AudioTrack'),
+        'Video': mocker.patch('subedit.factory.VideoTrack'),
+        'Subtitle': mocker.patch('subedit.factory.SubtitleTrack'),
+        'run': mocker.patch('subedit.factory.run'),
+        'JSONReader': mocker.patch('subedit.factory.JSONReader'),
+    }
+
+
+@fixture
+def mkvmerge_builder(mocker):
+    return mocker.patch('subedit.factory.MKVMergeAssetBuilder')
+
+
+def test_MKVMergeFactory_run_called(mkvmerge_deps, mkvmerge_builder):
+    expect_args = ['mkvmerge', '-J', 'video.mkv']
+    mkvmerge_factory = MKVMergeFactory(Path('video.mkv'))
+    mkvmerge_factory.build()
+    args, kwargs = mkvmerge_deps['run'].call_args
+
+    assert args[0] == expect_args
+    assert kwargs["stdout"] is PIPE
+
+
+def test_MKVMergeFactory_register_attachment(mkvmerge_deps, mkvmerge_builder):
+    mkvmerge_factory = MKVMergeFactory(Path('video.mkv'))
+    asset = mkvmerge_factory.build()
+    asset.register_type.assert_any_call(
+        mkvmerge_deps['AttachmentAdapter'],
+        mkvmerge_deps['Attachment'],
+        MediaType.ATTACHMENT
+    )
+
+
+def test_MKVMergeFactory_register_audio(mkvmerge_deps, mkvmerge_builder):
+    mkvmerge_factory = MKVMergeFactory(Path('video.mkv'))
+    asset = mkvmerge_factory.build()
+    asset.register_type.assert_any_call(
+        mkvmerge_deps['TrackAdapter'],
+        mkvmerge_deps['Audio'],
+        MediaType.AUDIO
+    )
+
+
+def test_MKVMergeFactory_register_video(mkvmerge_deps, mkvmerge_builder):
+    mkvmerge_factory = MKVMergeFactory(Path('video.mkv'))
+    asset = mkvmerge_factory.build()
+    asset.register_type.assert_any_call(
+        mkvmerge_deps['TrackAdapter'],
+        mkvmerge_deps['Video'],
+        MediaType.VIDEO
+    )
+
+
+def test_MKVMergeFactory_register_subtitle(mkvmerge_deps, mkvmerge_builder):
+    mkvmerge_factory = MKVMergeFactory(Path('video.mkv'))
+    asset = mkvmerge_factory.build()
+    asset.register_type.assert_any_call(
+        mkvmerge_deps['TrackAdapter'],
+        mkvmerge_deps['Subtitle'],
+        MediaType.SUBTITLE
+    )
