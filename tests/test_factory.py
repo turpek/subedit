140e97699cdba6eddf6408fdfd67a1a9f509eca6
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2025-12-31 14:22:40 -0300

    test: testar a classe `Factory`

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,7 +1,7 @@
 from pathlib3x import Path
 from pytest import fixture
-from subedit.factory import MKVMergeFactory
-from subedit.utils import MediaType
+from subedit.factory import Factory, MKVMergeFactory
+from subedit.utils import MediaType, Provider
 from subprocess import PIPE
 
 
@@ -22,6 +22,30 @@ def mkvmerge_deps(mocker):
 @fixture
 def mkvmerge_builder(mocker):
     return mocker.patch('subedit.factory.MKVMergeAssetBuilder')
+def test_Factory_build_mkvmerge_parse(mocker):
+    FakeFactory = mocker.MagicMock()
+    mocker.patch.dict(Factory._provider, {Provider.MKVMERGE: FakeFactory})
+    Factory.build_parse(Path('video.mkv'), Provider.MKVMERGE)
+
+
+def test_Factory_build_mkvmerge_parse_build(mocker):
+    FakeFactory = mocker.MagicMock()
+    fake_factory = mocker.MagicMock()
+    FakeFactory.return_value = fake_factory
+    mocker.patch.dict(Factory._provider, {Provider.MKVMERGE: FakeFactory})
+    Factory.build_parse(Path('video.mkv'), Provider.MKVMERGE)
+    fake_factory.build.assert_called()
+
+
+def test_Factory_build_return(mocker):
+    expect = 'asset_builder'
+    FakeFactory = mocker.MagicMock()
+    fake_factory = mocker.MagicMock()
+    fake_factory.build.return_value = expect
+    FakeFactory.return_value = fake_factory
+    mocker.patch.dict(Factory._provider, {Provider.MKVMERGE: FakeFactory})
+    result = Factory.build_parse(Path('video.mkv'), Provider.MKVMERGE)
+    assert result == expect
 
 
 def test_MKVMergeFactory_run_called(mkvmerge_deps, mkvmerge_builder):
