16ad2a79647a4ee626494ecd97e2cee43aa0d93d
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-01-06 16:52:16 -0300

    test: adicionar os testes para `TrackProperty`

--- tests/test_media_enums.py
+++ tests/test_media_enums.py
@@ -1,8 +1,9 @@
-from subedit.media_enums import MediaType, MEDIA_EXPANSION
+from subedit.media_enums import MediaType, MEDIA_EXPANSION, TrackProperty
 import pytest
 
 
 MT_VALUES = {'attachments', 'audio', 'video', 'subtitles', 'tracks', 'asset'}
+TP_VALUES = {'id', 'language', 'language_ietf', 'track_name', 'codec', 'codec_id'}
 
 
 MT_EXPANSION = {
@@ -22,3 +23,7 @@ def test_MediaType_members_values():
 @pytest.mark.parametrize('key,value', MT_EXPANSION.items())
 def test_MediaType_expansion(key, value):
     assert set(value) == set({member.value for member in MEDIA_EXPANSION[MediaType(key)]})
+
+
+def test_TrackProperty_members_values():
+    assert TP_VALUES == {member.value for member in TrackProperty}
